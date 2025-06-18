b0d035e1fb458be0a03212058d79e7e268f264b2
Warren Harding <warrencharding@yahoo.com>
2025-06-18 15:11:43 -0600

    Add src/SwarmGPUTest/Test1.cs

---
+++ src/SwarmGPUTest/Test1.cs
@@ -0,0 +1,12 @@
+//Copyright Warren Harding 2025.
+namespace SwarmGPUTest
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
